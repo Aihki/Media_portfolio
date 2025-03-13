b859c3a827dafa597c0cec95f3f138abcff632d0
aihki <laine.nestori.julius@gmail.com>
2025-03-13 08:44:37 +0200

    adding more error logs

--- backend-api/src/routes.rs
+++ backend-api/src/routes.rs
@@ -60,11 +60,10 @@ pub fn create_routes(db: Arc<Database>) -> Router {
         .route("/api/models/:id", delete(models::delete_model))
         .route("/api/photos/:id", delete(photos::delete_photo))
         .route("/api/videos/:id", delete(videos::delete_video))
-
+        .route("/api/model-file/:filename", get(models::get_file))
 
         .nest_service("/static/photos", ServeDir::new(photos::PHOTO_FOLDER))
         .nest_service("/static/videos", ServeDir::new(videos::VIDEO_FOLDER))
-        .nest_service("/static/models", ServeDir::new(models::MODEL_FOLDER))
         .nest_service("/static", ServeDir::new("static"))     
         .layer(cors)
         .layer(DefaultBodyLimit::max(1024 * 1024 * 500))
